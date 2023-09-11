62045353d60a7b4822f4701f69dbd6f7cabbeb6a
Shijo Joseph <shijocj@yahoo.com>
2023-09-10 23:59:56 -0400

    adding a new feature which if required will seperate the login information from library and avoid exposing them to wires everytime the api is used.

--- robin_stocks/robinhood/authentication.py
+++ robin_stocks/robinhood/authentication.py
@@ -49,6 +49,61 @@ def respond_to_challenge(challenge_id, sms_code):
     }
     return(request_post(url, payload))
 
+def inject_auth_token(data,pickle_name=""):
+    home_dir = os.path.expanduser("~")
+    data_dir = os.path.join(home_dir, ".tokens")
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+    creds_file = "robinhood" + pickle_name + ".pickle"
+    pickle_path = os.path.join(data_dir, creds_file)
+    if 'access_token' in data:
+        token = '{0} {1}'.format(data['token_type'], data['access_token'])
+        update_session('Authorization', token)
+        set_login_state(True)
+        data['detail'] = "logged in with brand new authentication code."
+        with open(pickle_path, 'wb') as f:
+                    pickle.dump({'token_type': data['token_type'],
+                                 'access_token': data['access_token'],
+                                 'refresh_token': data['refresh_token'],
+                                 'device_token': data['device_token']}, f)
+    else:
+        raise Exception(data['detail'])
+    
+
+def extract_auth_token(pickle_name=""):
+    home_dir = os.path.expanduser("~")
+    data_dir = os.path.join(home_dir, ".tokens")
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+    creds_file = "robinhood" + pickle_name + ".pickle"
+    pickle_path = os.path.join(data_dir, creds_file)
+    try:
+        with open(pickle_path, 'rb') as f:
+            pickle_data = pickle.load(f)
+            access_token = pickle_data['access_token']
+            token_type = pickle_data['token_type']
+            refresh_token = pickle_data['refresh_token']
+            # Set device_token to be the original device token when first logged in.
+            pickle_device_token = pickle_data['device_token']
+            payload['device_token'] = pickle_device_token
+            # Set login status to True in order to try and get account info.
+            set_login_state(True)
+            update_session(
+                'Authorization', '{0} {1}'.format(token_type, access_token))
+            # Try to load account profile to check that authorization token is still valid.
+            res = request_get(
+                positions_url(), 'pagination', {'nonzero': 'true'}, jsonify_data=False)
+            # Raises exception is response code is not 200.
+            res.raise_for_status()
+            return({'access_token': access_token, 'token_type': token_type,
+                    'expires_in': expiresIn, 'scope': scope, 'detail': 'logged in using authentication in {0}'.format(creds_file),
+                    'backup_code': None, 'refresh_token': refresh_token,'device_token': pickle_device_token})
+    except:
+        print(
+            "ERROR: There was an issue loading pickle file. Authentication may be expired - logging in normally.", file=get_output())
+        set_login_state(False)
+        update_session('Authorization', None)
+
 
 def login(username=None, password=None, expiresIn=86400, scope='internal', by_sms=True, store_session=True, mfa_code=None, pickle_name=""):
     """This function will effectively log the user into robinhood by getting an
@@ -132,7 +187,7 @@ def login(username=None, password=None, expiresIn=86400, scope='internal', by_sm
                     res.raise_for_status()
                     return({'access_token': access_token, 'token_type': token_type,
                             'expires_in': expiresIn, 'scope': scope, 'detail': 'logged in using authentication in {0}'.format(creds_file),
-                            'backup_code': None, 'refresh_token': refresh_token})
+                            'backup_code': None, 'refresh_token': refresh_token,'device_token': pickle_device_token})
             except:
                 print(
                     "ERROR: There was an issue loading pickle file. Authentication may be expired - logging in normally.", file=get_output())
@@ -188,6 +243,7 @@ def login(username=None, password=None, expiresIn=86400, scope='internal', by_sm
                                  'device_token': payload['device_token']}, f)
         else:
             raise Exception(data['detail'])
+        data['device_token'] = payload['device_token']
     else:
         raise Exception('Error: Trouble connecting to robinhood API. Check internet connection.')
     return(data)
