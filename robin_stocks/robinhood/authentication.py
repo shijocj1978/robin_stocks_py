e02879fa3165201be46d883a84ec313c9a6ce2d8
Shijo Joseph <shijocj@yahoo.com>
2023-09-11 22:06:48 -0400

    Tested and working external login part. extract_auth_token, inject_auth_token.

--- robin_stocks/robinhood/authentication.py
+++ robin_stocks/robinhood/authentication.py
@@ -62,12 +62,10 @@ def inject_auth_token(data,pickle_name=""):
         set_login_state(True)
         data['detail'] = "logged in with brand new authentication code."
         with open(pickle_path, 'wb') as f:
-                    pickle.dump({'token_type': data['token_type'],
-                                 'access_token': data['access_token'],
-                                 'refresh_token': data['refresh_token'],
-                                 'device_token': data['device_token']}, f)
+                    pickle.dump(data, f)
     else:
         raise Exception(data['detail'])
+    return data
     
 
 def extract_auth_token(pickle_name=""):
@@ -80,29 +78,24 @@ def extract_auth_token(pickle_name=""):
     try:
         with open(pickle_path, 'rb') as f:
             pickle_data = pickle.load(f)
+            # Set login status to True in order to try and get account info.
             access_token = pickle_data['access_token']
             token_type = pickle_data['token_type']
-            refresh_token = pickle_data['refresh_token']
-            # Set device_token to be the original device token when first logged in.
-            pickle_device_token = pickle_data['device_token']
-            payload['device_token'] = pickle_device_token
-            # Set login status to True in order to try and get account info.
             set_login_state(True)
-            update_session(
-                'Authorization', '{0} {1}'.format(token_type, access_token))
+            update_session('Authorization', '{0} {1}'.format(token_type, access_token))
             # Try to load account profile to check that authorization token is still valid.
-            res = request_get(
-                positions_url(), 'pagination', {'nonzero': 'true'}, jsonify_data=False)
+            res = request_get(positions_url(), 'pagination', {'nonzero': 'true'}, jsonify_data=False)
             # Raises exception is response code is not 200.
             res.raise_for_status()
             return({'access_token': access_token, 'token_type': token_type,
-                    'expires_in': expiresIn, 'scope': scope, 'detail': 'logged in using authentication in {0}'.format(creds_file),
-                    'backup_code': None, 'refresh_token': refresh_token,'device_token': pickle_device_token})
+                    'expires_in': pickle_data['expiresIn'], 'scope': pickle_data['scope'], 'detail': 'logged in using authentication in {0}'.format(creds_file),
+                    'backup_code': None, 'refresh_token': pickle_data['refresh_token'],'device_token': pickle_data['device_token']})
     except:
         print(
             "ERROR: There was an issue loading pickle file. Authentication may be expired - logging in normally.", file=get_output())
         set_login_state(False)
         update_session('Authorization', None)
+        return None
 
 
 def login(username=None, password=None, expiresIn=86400, scope='internal', by_sms=True, store_session=True, mfa_code=None, pickle_name=""):
@@ -240,7 +233,9 @@ def login(username=None, password=None, expiresIn=86400, scope='internal', by_sm
                     pickle.dump({'token_type': data['token_type'],
                                  'access_token': data['access_token'],
                                  'refresh_token': data['refresh_token'],
-                                 'device_token': payload['device_token']}, f)
+                                 'device_token': payload['device_token'],
+                                 'expires_in': expiresIn,
+                                 'scope': scope}, f)
         else:
             raise Exception(data['detail'])
         data['device_token'] = payload['device_token']
